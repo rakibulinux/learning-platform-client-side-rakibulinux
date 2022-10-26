3ecf5c4437895af7984dac1503141037bf4e12e2
Rakib Linux <rakibulinux@gmail.com>
2022-10-26 11:23:52 +0600

    export to pdf func added

--- src/components/CourseDetails.js
+++ src/components/CourseDetails.js
@@ -1,21 +1,57 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import LeftSide from "./LeftSide";
+// import { renderToString } from "react-dom/server";
+import jsPDF from "jspdf";
+// import ExportToPDF from "./ExportToPDF";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { html2canvas } from "html2canvas";
+import ExportToPDF from "./ExportToPDF";
 
 const CourseDetails = () => {
   const courseInfo = useLoaderData();
   console.log(courseInfo);
   const { title, image_url, details } = courseInfo;
+  // const print = () => {
+  //   const string = renderToString(
+  //     <Router>
+  //       <ExportToPDF courseInfo={courseInfo} />
+  //     </Router>
+  //   );
+  //   const pdf = new jsPDF("p", "mm", "a4");
+  //   // pdf.html2canvas(string);
+  //   pdf.save(`${title}`);
+  // };
+
+  const inputRef = useRef(null);
+  const printDocument = () => {
+    html2canvas(inputRef.current).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, "JPEG", 0, 0);
+      pdf.save("download.pdf");
+    });
+  };
+
   return (
     <div className="courses">
       <div className="">
         <LeftSide />
       </div>
+
       <div className="grid grid-cols-1 gap-4">
-        <button className="bg-cyan-900 text-cyan-50 font-semibold py-2 w-32 mx-auto rounded-md">
+        <button
+          onClick={printDocument}
+          className="bg-cyan-900 text-cyan-50 font-semibold py-2 w-32 mx-auto rounded-md"
+        >
           Export to PDF
         </button>
-        <div className="flex justify-center flex-col">
+        <ExportToPDF courseInfo={courseInfo} />
+        {/* <div
+          id="divToPrint"
+          ref={inputRef}
+          className="flex justify-center flex-col"
+        >
           <img src={image_url} alt="" />
           <div>
             <h2 className="text-3xl my-5">{title}</h2>
@@ -26,7 +62,7 @@ const CourseDetails = () => {
               </button>
             </Link>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
---
+++ src/components/ExportToPDF.js
@@ -0,0 +1,38 @@
+import { useRef } from "react";
+import { html2canvas } from "html2canvas";
+import { jsPDF } from "jspdf";
+
+const ExportToPDF = () => {
+  const inputRef = useRef(null);
+  const printDocument = () => {
+    const input = document.getElementById("divToPrint");
+    html2canvas(input, {
+      logging: true,
+      letterRendering: 1,
+      useCORS: true,
+    }).then((canvas) => {
+      const imgWidth = 208;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      const imgData = canvas.toDataURL("img/png");
+      const pdf = new jsPDF("p", "mm", "a4");
+      pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+      pdf.save("download.pdf");
+    });
+  };
+  return (
+    <>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <div className="mb5">
+          <button onClick={() => printDocument()}>Print</button>
+        </div>
+        <div id="divToPrint" ref={inputRef}>
+          <div>Note: Here the dimensions of div are same as A4</div>
+          <div>You Can add any component here</div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default ExportToPDF;
